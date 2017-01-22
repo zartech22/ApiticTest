d6700d0526b5df3d1864ca229b6a22aea2cf0bb7
Kevin Plestan <kevin.plestan@outlook.fr>
2017-01-22 22:42:54 +0100

    Add comments

--- src/CoreBundle/DataFixtures/ORM/LoadAnimal.php
+++ src/CoreBundle/DataFixtures/ORM/LoadAnimal.php
@@ -9,6 +9,7 @@
 namespace CoreBundle\DataFixtures\ORM;
 
 
+use CoreBundle\CoreBundle;
 use CoreBundle\Entity\AnimalType;
 use CoreBundle\Entity\Bird;
 use CoreBundle\Entity\Mammal;
@@ -16,20 +17,40 @@ use CoreBundle\Entity\Reptile;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
+/**
+ * Class LoadAnimal
+ *
+ * Class which loads a set of examples animals data into the database
+ * It loads Mammals, Reptiles and Birds
+ *
+ * @package CoreBundle\DataFixtures\ORM
+ * @see \CoreBundle\Entity\Animal
+ * @see \CoreBundle\Entity\Mammal
+ * @see \CoreBundle\Entity\Reptile
+ * @see \CoreBundle\Entity\Bird
+ */
 class LoadAnimal implements FixtureInterface
 {
+    // List of possible names to use to fill the database
     private static $names = array('Jean', 'Fifi', 'Roberta', 'Pikachu', 'Doctor Who', 'Yvette', 'Plumesec', 'Syndra',
         'Varian', 'Fordring', 'Karma', 'Neltharion');
+
+    // Lists of adjectives for the fur, the scale and the feathers of animals
     private static $furDescriptions = array('chatoyante', 'douce', 'aux longs poils');
     private static $scaleDescriptions = array('coupantes', 'lisses', 'résistantes');
     private static $feathersDescriptions = array('doux', 'long', 'sombre');
 
+    // Lists of species
     private static $growls = array('lion', 'girafe', 'chaton');
     private static $hisses = array('couleuvre', 'cobra', 'crocodile');
     private static $tweets = array('pivert', 'hiboux', 'phoenix', 'aigle');
 
     /**
      * Load data fixtures with the passed EntityManager
+     *
+     * Use shuffled data within lists to fill entities.
+     * Data are the name, an adjective for an animal characteristic (fur, scale or feather)
+     * and a species for each animal category (mammal, reptile and bird)
      *
      * @param ObjectManager $manager
      */
@@ -57,6 +78,11 @@ class LoadAnimal implements FixtureInterface
         $manager->flush();
     }
 
+    /**
+     * Fill an array with Mammal
+     * @param array $set The array to be filled
+     * @see \CoreBundle\Entity\Mammal
+     */
     private function loadMammals(array &$set)
     {
         $type = new AnimalType();
@@ -74,6 +100,11 @@ class LoadAnimal implements FixtureInterface
         }
     }
 
+    /**
+     * Fill an array with Reptiles
+     * @param array $set The array to be filled
+     * @see \CoreBundle\Entity\Reptile
+     */
     private function loadReptiles(array &$set)
     {
         $type = new AnimalType();
@@ -91,6 +122,11 @@ class LoadAnimal implements FixtureInterface
         }
     }
 
+    /**
+     * Fill an array with Bird
+     * @param array $set The array to be filled
+     * @see \CoreBundle\Entity\Bird
+     */
     private function loadBirds(array &$set)
     {
         $type = new AnimalType();
