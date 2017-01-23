f1711105b6832b34c40c5ed6224be6236a1fb431
Kevin Plestan <kevin.plestan@outlook.fr>
2017-01-23 15:19:03 +0100

    Complete add/edit/delete actions

--- src/CoreBundle/Controller/CoreController.php
+++ src/CoreBundle/Controller/CoreController.php
@@ -2,6 +2,8 @@
 
 namespace CoreBundle\Controller;
 
+use CoreBundle\Entity\Animal;
+use CoreBundle\Form\AnimalType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -16,16 +18,42 @@ class CoreController extends Controller
 
     public function addAction(Request $request)
     {
+        $animal = new Animal();
+        $form = $this->createForm(AnimalType::class, $animal);
 
+        if($form->handleRequest($request)->isValid())
+        {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($animal);
+            $manager->flush();
+
+            return $this->redirectToRoute('core_homepage');
+        }
+
+        return $this->render('CoreBundle:Core:add.html.twig', array('form' => $form->createView()));
     }
 
-    public function editAction($id, Request $request)
+    public function editAction(Animal $animal, Request $request)
     {
+        $form = $this->createForm(AnimalType::class, $animal);
 
+        if($form->handleRequest($request)->isValid())
+        {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('core_homepage');
+        }
+
+        return $this->render('CoreBundle:Core:edit.html.twig', array('form' => $form->createView()));
     }
 
-    public function deleteAction($id)
+    public function deleteAction(Animal $animal)
     {
+        $this->getDoctrine()->getManager()->remove($animal);
+        $this->getDoctrine()->getManager()->flush();
 
+        $this->addFlash('success', 'Animal correctement supprimé !');
+
+        return $this->redirectToRoute('core_homepage');
     }
 }
