5d52e784812749a5bfa7e2e709889475c67ed19a
Kevin Plestan <kevin.plestan@outlook.fr>
2017-01-23 15:37:50 +0100

    Add comments

--- src/CoreBundle/DoctrineListener/AnimalSpecializer.php
+++ src/CoreBundle/DoctrineListener/AnimalSpecializer.php
@@ -16,8 +16,21 @@ use CoreBundle\Entity\Reptile;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 
+/**
+ * Doctrine's Listener class.
+ *
+ * Handles Animal's persists and updates by finding which subclass the entity is and
+ * transform it to it's subclass. Listen to 'onFlush' and 'prePersist'
+ *
+ * Class AnimalSpecializer
+ * @package CoreBundle\DoctrineListener
+ */
 class AnimalSpecializer
 {
+    /**
+     * Prevents Animal's entity to be persisted or updated
+     * @param OnFlushEventArgs $args
+     */
     public function onFlush(OnFlushEventArgs $args)
     {
         $unitOfWork = $args->getEntityManager()->getUnitOfWork();
@@ -31,6 +44,11 @@ class AnimalSpecializer
                 $unitOfWork->detach($entity);
     }
 
+    /**
+     * Get the entity and if it is an Animal entity persist an Animal's subclass
+     * based on its data depending of its type
+     * @param LifecycleEventArgs $args
+     */
     public function prePersist(LifecycleEventArgs $args)
     {
         $entity = $args->getObject();
