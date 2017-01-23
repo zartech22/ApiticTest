7c9f1d3ff9b096789d11744dee00a25eeb5abf82
Kevin Plestan <kevin.plestan@outlook.fr>
2017-01-23 15:24:17 +0100

    Remove $id from Animal's subclass and redirect 'getId()' method to animal's 'getId()'

--- src/CoreBundle/Entity/Reptile.php
+++ src/CoreBundle/Entity/Reptile.php
@@ -13,23 +13,13 @@ use Doctrine\ORM\Mapping as ORM;
 class Reptile extends Animal
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
-
-
-    /**
      * Get id
      *
      * @return integer 
      */
     public function getId()
     {
-        return $this->id;
+        return parent::getId();
     }
 
     public function scale()
