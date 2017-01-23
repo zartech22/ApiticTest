616a3834beeeab6bf6e346f6d54ac5628f43013b
Kevin Plestan <kevin.plestan@outlook.fr>
2017-01-24 00:44:37 +0100

    Updates submit button's label when user edit an Animal

--- src/CoreBundle/Form/AnimalType.php
+++ src/CoreBundle/Form/AnimalType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AnimalType extends AbstractType
@@ -21,6 +23,27 @@ class AnimalType extends AbstractType
             ->add('species', TextType::class, array('label' => "Espèce de l'animal"))
             ->add('description', TextType::class, array('label' => "Description des écailles / fourrure / plumage de l'animal"))
             ->add('save', SubmitType::class, array('label' => 'Ajouter'));
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'preSetDataListener'));
+    }
+
+    /**
+     * Listen PRE_SET_DATA form event to change the submit button label if user edits an Animal instead of adding ir
+     * @param FormEvent $event
+     */
+    public function preSetDataListener(FormEvent $event)
+    {
+        $data = $event->getData();
+
+        if($data === null)
+            return;
+
+        if($data->getId() !== null)
+        {
+            $options = $event->getForm()->get('save')->getConfig()->getOptions();
+
+            $event->getForm()->add('save', SubmitType::class, array_replace($options, array('label' => 'Mettre à jour')));
+        }
     }
 
     /**
